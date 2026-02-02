3475104da027d5be3a6056b464eaa4e9c2418118
Augusto Brito <augusstobrito@gmail.com>
2026-02-02 00:42:10 -0300

    Set products store and render cart items

--- src/components/home/pizza-list.tsx
+++ src/components/home/pizza-list.tsx
@@ -1,10 +1,19 @@
+"use client"
+
 import { Product } from "@/lib/generated/prisma/client"
 import { PizzaItem } from "./pizza-item"
+import { useProducts } from "@/stores/products"
+import { useEffect } from "react"
 
 type Props = {
     pizzas: Product[]
 }
 export const PizzaList = ({ pizzas }: Props) => {
+    const products = useProducts()
+
+    useEffect(() => {
+        products.setProducts(pizzas)
+    }, [pizzas])
 
     return (
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
--- src/components/layout/cart/Cart-Item.tsx
+++ src/components/layout/cart/Cart-Item.tsx
@@ -1,4 +1,4 @@
-"üse client"
+"use client"
 
 import { useCart } from "@/stores/cart"
 import { useProducts } from "@/stores/products"
@@ -8,14 +8,20 @@ export const CartItem = () => {
     const products = useProducts()
     return (
         <div>
-            {cart.items.length > 1 &&
-                cart.items.map((item) => 
-                    <div key={item.productId}>
-                    </div>
-                )
+            {cart.items.length > 0 &&
+                cart.items.map((item) => {
+                    const product = products.products.find(p => p.id === item.productId)
+                    if (!product) return null
+                    return (
+                        <div key={product.id}>
+                            <p>{product.name}</p>
+                            <p>Quantidade: {item.quantity}</p>
+                        </div>
+                    )
+                })
             }
             
-            {cart.items.length < 1 &&
+            {cart.items.length === 0 &&
                 <p>Nenhum item no carrinho.</p>
             }
         </div>
